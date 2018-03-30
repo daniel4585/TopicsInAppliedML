d42d99917b85d099bb55ff00adffefe4fbb012cd
levydaniel <dlevy4585@gmail.com>
2018-03-30 14:52:34 +0300

    AHMAD

---
+++ assignment1/Lambda.py
@@ -0,0 +1,13 @@
+
+
+class Lambda(object):
+    v = 1
+    u = 1
+    bu = 1
+    bv = 1
+
+    def __init__(self):
+        super(Lambda, self).__init__()
+
+
+
---
+++ assignment1/MFModel.py
@@ -0,0 +1,20 @@
+import numpy as np
+from Lambda import Lambda
+
+
+class MFModel(object):
+    K = 20
+
+    def __init__(self, R):
+        self.R = R
+        self.num_users, self.num_movies = R.shape
+        self.u = np.random.normal(scale=1. / self.K, size=(self.num_users, self.K))
+        self.v = np.random.normal(scale=1. / self.K, size=(self.num_movies, self.K))
+
+        self.b_n = np.zero(self.num_users)
+        self.b_m = np.zero(self.num_movies)
+
+        # Initialize mu by taking the average of existing ratings
+        self.mu = np.mean(R[np.where(R != 0)])
+
+        self.lamb = Lambda()
