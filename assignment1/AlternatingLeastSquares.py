a7c8ff807651a09e397adea8dd6547c4dc5da039
pelegn <pelegneta@gmail.com>
2018-04-07 08:48:47 +0300

    added pkl to git ignore +als initial

--- assignment1/AlternatingLeastSquares.py
+++ assignment1/AlternatingLeastSquares.py
@@ -1,33 +1,74 @@
 import numpy as np
 
 
-class Parameters(object, ):
-    def __init__(self, convergence_threshold):
-        super(Parameters, self).__init__()
+class ALSParameters(object):
+    def __init__(self, convergence_threshold, lamb):
+        super(ALSParameters, self).__init__()
         self.convergence_threshold = convergence_threshold
+        self.lamb = lamb
 
 
 
-def LearnModelFromDataUsingALS(data, mfmodel, parameters):
+def  LearnModelFromDataUsingALS(data, mfmodel, parameters):
 
     e = float("inf")
+    M , N  = data.shape
     while e > parameters.convergence_threshold:
-        xs, ys = data.nonzero()
-        for x, y in zip(xs, ys):
-            sample = (x, y, data[x, y])
-            alternating_least_squares_update(sample, mfmodel, parameters)
-
+        for n in range(N-1):
+            alternating_least_squares_update_vn(n, data, mfmodel, parameters)
+        for m in range(M-1):
+            alternating_least_squares_update_um(m, data, mfmodel, parameters)
         predicted = mfmodel.calc_matrix()
         e = mfmodel.mean_squared_error(predicted)
         print("Error: %f" % e)
 
 
 
-def alternating_least_squares_update(sample, mfmodel, parameters):
+def  alternating_least_squares_update_vn(n,data, mfmodel, parameters):
+
+    y = data[:, n]
+    X = mfmodel.u.T
+
+    # print 'y: {0} X: {1}'.format(y.shape, X.shape)
+    # print 'inv: {0}'.format(np.linalg.inv(X.dot(X.T) + parameters.lamb * np.eye(X.shape[0])).shape)
+    # print "X'y: {0}".format(X.dot(y).shape)
+    # print 'full: {0}'.format(np.squeeze(np.linalg.inv(X.dot(X.T) + parameters.lamb * np.eye(X.shape[0])).dot(X.dot(y))).shape)
+
+    # Closed-form solution for the j'th ridge regression
+    mfmodel.v[n, :] = np.squeeze(np.linalg.inv(X.dot(X.T) + parameters.lamb * np.eye(X.shape[0])).dot(X.dot(y)))
 
 
+    # r_nb = data[:,n] - mfmodel.b_m - mfmodel.mu - mfmodel.b_n[n]
+    # # nonzeros = data[:, n].nonzero()[0]
+    # X = mfmodel.u[:,n].T
+    # y_n = mfmodel.v[:,n]
+    # tmp = X.T.dot(X) + parameters.lamb * np.eye(X.shape[0])
+    # tmp = np.linalg.inv(tmp)
+    # tmp2 = X.T.dot(r_nb)
+    # y_n = tmp.dot(tmp2)
+    # mfmodel.v[:, n] = y_n
 
-def calc_matrix(mfmodel):
+def  alternating_least_squares_update_um(m, data, mfmodel, parameters):
+    y = data[m, :]
+    X = mfmodel.v.T
+
+    # print 'y: {0} X: {1}'.format(y.shape, X.shape)
+    # print 'inv: {0}'.format(np.linalg.inv(X.dot(X.T) + parameters.lamb * np.eye(X.shape[0])).shape)
+    # print "X'y: {0}".format(X.dot(y).shape)
+    # print 'full: {0}'.format(
+    # np.squeeze(np.linalg.inv(X.dot(X.T) + parameters.lamb * np.eye(X.shape[0])).dot(X.dot(y))).shape)
+
+    # Closed-form solution for the j'th ridge regression
+    mfmodel.u[m, :] = np.squeeze(np.linalg.inv(X.dot(X.T) + parameters.lamb * np.eye(X.shape[0])).dot(X.dot(y)))
+
+    # r_mb = data[:,m] - mfmodel.b_n - mfmodel.mu - mfmodel.b_m[m]
+    # # nonzeros = data[:, n].nonzero()[0]
+    # Y =  mfmodel.v[:, m].T
+    # X_m = mfmodel.u[:, m]
+    # X_m = np.linalg.inv(Y.T.dot(Y) + parameters.lamb * np.eye(Y.shape[1])).dot(Y.T.dot(r_mb))
+    # mfmodel.u[:, m] = X_m
+
+def  calc_matrix(mfmodel):
     return (mfmodel.mu + mfmodel.b_n[:, np.newaxis] + mfmodel.b_m[np.newaxis, :]).T + mfmodel.u.dot(mfmodel.v.T)
 
 
--- assignment1/main.py
+++ assignment1/main.py
@@ -1,28 +1,38 @@
 from MFModel import MFModel
 from GradientDecent import LearnModelFromDataUsingSGD, Parameters
 from Lambda import Lambda
+from AlternatingLeastSquares import LearnModelFromDataUsingALS, ALSParameters
 from utils import *
-
 from Movie import Movie
 from User import User
 from Rating import Rating
+import pickle
+
 
 NUM_USERS = 6041  # One more than actual number of Users
 NUM_MOVIES = 3953  # One more than actual number of Movies
 
 
 def main():
-    movies = extract_data("data/movies.dat", Movie)
-    users = extract_data("data/users.dat", User)
-    ratings = extract_data("data/ratings.dat", Rating)
-    train, test = train_test_split(ratings)
-    R = create_data_matrix(train, NUM_USERS, NUM_MOVIES)
+    # movies = extract_data("data/movies.dat", Movie)
+    # users = extract_data("data/users.dat", User)
+    # ratings = extract_data("data/ratings.dat", Rating)
+    # train, test = train_test_split(ratings)
+    # R = create_data_matrix(train, NUM_USERS, NUM_MOVIES)
+    #
+    # with open('R.pkl', 'wb') as output:
+    #     pickle.dump(R, output, pickle.HIGHEST_PROTOCOL)
+
+
+    with open('R.pkl', 'rb') as input:
+        R = pickle.load(input)
 
     regularization = 0.2
     lamb = Lambda(lambda_u=regularization, lambda_v=regularization, lambda_b_u=regularization, lambda_b_v=regularization)
     model = MFModel(R, K=20, lamb=lamb)
 
-    LearnModelFromDataUsingSGD(R, model, Parameters(steps=10, alpha=0.1))
+    # LearnModelFromDataUsingSGD(R, model, Parameters(steps=10, alpha=0.1))
+    LearnModelFromDataUsingALS(R, model, ALSParameters(convergence_threshold=10, lamb=0.1))
 
 
 if __name__ == '__main__':
